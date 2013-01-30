31839b2959ee14bec363243b613f95f466e46dfe
cloudaice <cloudaice@163.com>
2013-01-30 20:48:44 +0800

    start chengji parse

---
+++ parsehtml/parse_chengji.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+from bs4 import BeautifulSoup as BS
+import re
+import sys
+import escap
+
+
+class Chengji(object):
+    def __init__(self, html):
+        self.html = html
+        self.soup = BS(html)
+        self.table_title = None
+
