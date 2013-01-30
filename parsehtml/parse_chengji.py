b388ff8aba4bfe872bb302b823608e4ee5717323
cloudaice <cloudaice@163.com>
2013-01-31 01:46:45 +0800

    :v: do chengji parse

--- parsehtml/parse_chengji.py
+++ parsehtml/parse_chengji.py
@@ -19,12 +19,32 @@ class Chengji(object):
        div = self.soup.html.find(id='spacemain').find(class_='center')
        div = filter(lambda x: hasattr(x, 'name') and x.name == 'table', div.contents)
        table = filter(lambda x: hasattr(x, 'name'), div[0].contents)
-       self.student_num = table[0].stripped_strings.next().split()[0][0:-1]
-      
+       try:
+           self.student_num = table[0].stripped_strings.next().split()[0][0:-1]
+       except StopIteration, e:
+           print 'can not get student number'
+           pass
+
        table = filter(lambda x: hasattr(x, 'name'), div[1].contents)
-       table = filter(lambda x: hasattr(x, 'name'), table[1].contents)
+       table = filter(lambda x: hasattr(x, 'name'), table[0].contents)
+       for row in table:
+           this_row = ['' for _ in range(10)]
+           row_iter = row.stripped_strings
+           try:
+               tern = row_iter.next()
+           except StopIteration, e:
+               continue
+           this_row[0] = tern
+           for i in range(1,10,1):
+               try:
+                   this_row[i] = row_iter.next()
+               except StopIteration, e:
+                   continue
+           self.chengji.append(tuple(this_row))
 
-
+    def show(self):
+        for row in self.chengji:
+            print "%s," * 10  % row
 
 if __name__ == "__main__":
     with open(current_dir + 'chengji.html', 'r') as html:
@@ -32,5 +52,6 @@ if __name__ == "__main__":
         html = escap.escap(html)
         html = Chengji(html)
     html()
+    html.show()
 
 
