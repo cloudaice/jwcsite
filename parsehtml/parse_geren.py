1f119140ff50685dc1e3545f3ffc69c677691e55
cloudaice <cloudaice@163.com>
2013-02-03 00:07:12 +0800

    add parse_geren.py

---
+++ parsehtml/parse_geren.py
@@ -0,0 +1,25 @@
+#-*-coding:utf-8-*-
+from bs4 import BeautifulSoup as BS
+import sys
+import escap
+current_dir = sys.path[0] + '/'
+
+
+class Geren(object):
+    def __init__(self, html):
+        self.html = html
+        self.soup = BS(html)
+        self.student_num = None
+        self.profile = []
+
+    def __call__(self):
+        forms = self.soup.find('form')
+        print forms
+        
+
+if __name__ == "__main__":
+    with open(current_dir + 'geren.html') as html:
+        html = html.read()
+        html = escap.escap(html)
+        html = Geren(html)
+    html()
