4ee47127ae6953dd7947d903d8aa134394797c15
cloudaice <cloudaice@163.com>
2013-02-03 00:35:40 +0800

    complete:v:

--- parsehtml/parse_geren.py
+++ parsehtml/parse_geren.py
@@ -10,12 +10,31 @@ class Geren(object):
         self.html = html
         self.soup = BS(html)
         self.student_num = None
-        self.profile = []
+        self.profile = {}
 
     def __call__(self):
         forms = self.soup.find('form')
         tbody = forms.find('tbody')
         trs = filter(lambda x: hasattr(x, 'name') and x.name == 'tr', tbody.contents)
+        for i, tr in enumerate(trs):
+            if i == 6:
+                continue
+            tr_iter = tr.stripped_strings
+            while True:
+                try:
+                    key = tr_iter.next()
+                    if u'姓名拼音' in key:
+                        key = key + tr_iter.next()
+                    if i == 7:
+                        value = tr.find('input').get('value')
+                        print value
+                    else:
+                        value = tr_iter.next()
+                    self.profile[key] = value
+                except StopIteration:
+                    break
+        for key in self.profile.keys():
+            print key, self.profile[key]
 
 if __name__ == "__main__":
     with open(current_dir + 'geren.html') as html:
