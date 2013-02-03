5daa632fd23981582277c138c34a027d7261db41
cloudaice <cloudaice@163.com>
2013-02-03 23:20:06 +0800

    抽象下载类

--- req.py
+++ req.py
@@ -5,6 +5,26 @@ import config
 sys.path.append(sys.path[0] + '/parsehtml')
 import parse_geren
 
+
+class Do_html(object):
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+
+    def __call__(self):
+        r = requests.post(url, headers=headers)
+        r.encoding = 'GBK'
+        html = r.text
+        return html
+
+class Do_pic(object):
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+
+    def __call__(self):
+        r = requests.post(url, headers=headers)
+
 if __name__ == '__main__':
     url = config.url
     headers = config.headers
@@ -13,3 +33,4 @@ if __name__ == '__main__':
     html = r.text
     html = parse_geren.Geren(html)
     html()
+
