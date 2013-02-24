532c81d2be9b384d67dc54bd725e1e6887668a84
cloudaice <cloudaice@163.com>
2013-02-24 15:10:07 +0800

    down jiaoshi page

---
+++ parse/parse_jiaoshi.py
@@ -0,0 +1,16 @@
+#-*-coding: utf-8 -*-
+from bs4 import BeautifulSoup as BS
+import sys
+
+current_dir = sys.path[0] + '/'
+
+
+class Jiaoshi(object):
+    def __init__(self, html):
+        self.html = html
+        self.soup = BS(html)
+        self.rooms = []
+        assert(self.soup.name == '[document]')
+
+    def __call__(self):
+        div = self.soup.html.find(id='spacemain').find(calss_='center')
---
+++ test_jiaoshi.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append(sys.path[0] + '/parse')
+import config
+from parse_jiaosi import Jiaoshi
+from req import *
+
+
+if __name__ == '__main__':
+    parame = {'JS': '',
+              'ZC': '01',
+              'XQ': '一校区',
+              'submit2': '全部浏览'
+              }
