9da622bb026f9f11e8b32bdbefcc4f755794251f
cloudaice <cloudaice@163.com>
2013-02-24 16:33:04 +0800

    complete parse_jiaoshi

--- parse/parse_jiaoshi.py
+++ parse/parse_jiaoshi.py
@@ -15,4 +15,14 @@ class Jiaoshi(object):
     def __call__(self):
         div = self.soup.html.find(id='spacemain').find(class_='center')
         table = div.find('table', bgcolor='#FFFFFF')
-        print table
+        table = filter(lambda x: hasattr(x, 'name'), table.contents)
+        for row in table[2:]:
+            tds = filter(lambda x: hasattr(x, 'name') and x.name == 'td', row.contents)
+            room_name = tds[0].string.strip()
+            all_colors = []
+            for td in tds[1:]:
+                all_colors.append(td.get('bgcolor'))
+            week_status = ''.join(['0' if color == '#FFFFFF' else '1' for color in all_colors])
+            self.rooms.append((room_name, week_status))
+        for room, status in self.rooms:
+            print room, status
