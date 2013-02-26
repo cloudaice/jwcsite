18c01acb5eb3378db0d0f09b1874fd36840ac13d
cloudaice <cloudaice@163.com>
2013-02-27 02:45:24 +0800

    complete test

--- collector/geren/test_geren.py
+++ collector/geren/test_geren.py
@@ -5,9 +5,19 @@ sys.path.append(sys.path[0] + '/../../')
 import config
 from factory import CollectGeren
 from req import Get_html
+from pymongo import Collection
+import time
+
+cnn = Collection('localhost', 27018, max_pool_size=10)
+db = cnn['jwcsite']
+table = db.PersonInfo
 
 if __name__ == '__main__':
-    headers = config.headers_geren('1090310108')
-    url = config.url_geren
-    req = Get_html(url, headers)
-    collect = CollectGeren(req, '1090310108')
+    stuids = table.find()
+    for stuid in stuids:
+        stuid = stuid['stuid']
+        headers = config.headers_geren(stuid)
+        url = config.url_geren
+        req = Get_html(url, headers)
+        collect = CollectGeren(req, stuid)
+        time.sleep(0.5)
