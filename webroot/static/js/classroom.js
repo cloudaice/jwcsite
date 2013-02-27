9a6e5305b7e5c4cb133b7db5ea5506ea1552cc80
cloudaice <cloudaice@163.com>
2013-02-28 01:03:45 +0800

    can see

--- webroot/static/js/classroom.js
+++ webroot/static/js/classroom.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(document).ready(function(){
+    $('#buildTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+    var s = $('.nav-tabs').button();
+    console.debug('hello');
 });
