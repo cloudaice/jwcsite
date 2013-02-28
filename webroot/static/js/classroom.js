379ad2cca39f6375e4581add7963a3cdc5e97c0d
cloudaice <cloudaice@163.com>
2013-02-28 11:52:47 +0800

    can select

--- webroot/static/js/classroom.js
+++ webroot/static/js/classroom.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+    function check_checkbox(){
+
+    }
     $('#buildTab a').click(function (e) {
         e.preventDefault();
         $(this).tab('show');
+        var buildname = $(this).text();
+        console.debug($(this).text())
+
     });
+
     console.debug('hello');
 });
