c5e0594826e20c8f877e619a8e548f8cb870d604
cloudaice <cloudaice@163.com>
2013-03-12 14:59:47 +0800

    delete old jwc service

--- stu_service/admin.py
+++
@@ -1,6 +0,0 @@
-from stu_service.models import js_status,feedback_data,psd,class_table
-from django.contrib import admin
-
-admin.site.register(js_status)
-admin.site.register(feedback_data)
-admin.site.register(class_table)
