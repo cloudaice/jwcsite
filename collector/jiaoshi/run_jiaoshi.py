583a90a71adcbe549c15117ae9fbc3a3c1b38e4a
cloudaice <cloudaice@163.com>
2013-03-17 16:50:29 +0800

    complete run_jiaoshi.py

--- collector/jiaoshi/run_jiaoshi.py
+++ collector/jiaoshi/run_jiaoshi.py
@@ -5,10 +5,22 @@ sys.path.append(sys.path[0] + '/../../')
 import config
 from factory import CollectJiaoshi
 from req import Post_html
+from datetime import datetime
 
+
+def wrapweek(week):
+    if week < 10:
+        return '0' + str(week)
+    else:
+        return str(week)
 
 if __name__ == '__main__':
-    week = sys.argv[1].strip()
+    tern_start_day = config.tern_start_day
+    tern_start_day = [int(v) for v in tern_start_day.split('-')]
+    d1 = datetime(tern_start_day[0], tern_start_day[1], tern_start_day[2])
+    d2 = datetime.now()
+    days = (d2 - d1).days
+    week = wrapweek(((days) / 7 + 2))
     campus = '一校区'
     collect = CollectJiaoshi(week, campus)
     parame = {'JS': '',
