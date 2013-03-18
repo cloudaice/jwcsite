18da0fd231143ff941f6f489ccdf222d069b939c
cloudaice <cloudaice@163.com>
2013-03-18 10:46:51 +0800

    add highlight

--- webroot/static/js/base.js
+++ webroot/static/js/base.js
@@ -2,8 +2,17 @@ $(document).ready(function(){
     url = window.location.href;
     //$('#topTab').children().removeClass('active');
     lasturl = url.split('/');
-    classname = '/' + lasturl[lasturl.length-1];
-    //console.debug(classname);
-    //console.debug($("[href='" + classname + "']").text());
-    //$("[href='" + classname + "']").addClass('active');
+    classname = '/' + lasturl[lasturl.length-1]; 
+    if (classname == '/classroom'){
+        $('#top1').addClass('active');
+        console.debug(classname);
+    }
+    if (classname == '/curriculum'){
+        $('#top2').addClass('active');
+        console.debug(classname);
+    }
+    if (classname == '/about'){
+        $('#top3').addClass('active');
+        console.debug(classname);
+    }
 });
