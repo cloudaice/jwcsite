5b4b89fa59ef110be316c556afd3dabef8fb222e
cloudaice <cloudaice@163.com>
2013-04-06 21:14:35 +0200

    添加日志模块

---
+++ lib/log.py
@@ -0,0 +1,21 @@
+#-*-coding: utf-8-*-
+import logging
+import logging.handlers
+import os
+
+
+def get_logger(filename):
+    logger = logging.getLogger('jwcsite')
+    handler = logging.FileHandler(filename)
+    formatter = logging.Formatter('%(asctime)s - %(module)s.%(funcName)s.%(lineno)d - %(levelname)s - \
+        %(message)s')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
+
+logger = get_logger(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'jwcsite.log'))
+namepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'jwcsite.log')
+print os.path.abspath(__file__)
+print os.path.dirname(os.path.abspath(__file__))
+print namepath
